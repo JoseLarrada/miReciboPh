fc09c76257399422ea236c0c0ac92ea25fa4428c
Jose Larrada <jalarrada@unicesar.edu.co>
2025-07-03 09:01:02 -0500

    Primera configuracion de la base de datos local

---
+++ src/main/java/com/ph/mireciboph/MirecibophApplication.java
@@ -0,0 +1,13 @@
+package com.ph.mireciboph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MirecibophApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MirecibophApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ph/mireciboph/MirecibophApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ph.mireciboph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MirecibophApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
